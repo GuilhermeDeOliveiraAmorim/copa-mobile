c52afc06972b89bb5f9b179a047c8839e0b9044c
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-11-06 13:06:39 -0300

    init: mobile NLW

---
+++ src/screens/Signin.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from "native-base";
+
+export function Signin() {
+    return (
+        <Center flex={1} bgColor="black">
+            <Text color="white">Signin</Text>
+        </Center>
+    )
+}
