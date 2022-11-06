a77e606aeddf753501f92e8d1c3d3a572cc6811d
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-11-06 13:11:04 -0300

    feat: TOC

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { THEME } from "./src/styles/theme"
+import { THEME } from "./src/styles/theme";
 
 import { Loading } from './src/components/Loading';
 import { Signin } from './src/screens/Signin';
--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,9 +1,11 @@
 import { Center, Text } from "native-base";
 
-export function Signin() {
+function Signin() {
     return (
         <Center flex={1} bgColor="black">
             <Text color="white">Signin</Text>
         </Center>
     )
 }
+
+export { Signin };
